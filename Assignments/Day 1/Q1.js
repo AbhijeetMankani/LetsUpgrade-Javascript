3efef8c46fd120810e2fcf97dab999c958a9620f
AbhijeetSMankani <52568152+AbhijeetSMankani@users.noreply.github.com>
2020-07-15 22:27:01 +0530

    Add files via upload

---
+++ Assignments/Day 1/Q1.js
@@ -0,0 +1,19 @@
+// console.log() prints the provided arguments in the comsole, open console by pressing F12
+console.log("I love FCS");
+
+// console.warn() prints an warning in the console
+console.warn("This is a sample warning");
+
+// console.error() prints an error in the console
+console.error("This is a sample error");
+
+// document.write() writes a line in the page
+document.write("I love FCS");
+
+// window.alert() creates an alert
+window.alert("This is a sample alert");
+
+// document.getElementById() returns an instce of the DOM object with provided id
+p = document.getElementById('p1');
+// p.innerHTML changes the displayed text of the object
+p.innerHTML = "I love FCS";
---
+++ Assignments/Day 1/Q2.js
@@ -0,0 +1,20 @@
+document.writeln("Code1:<br>")
+document.writeln(`
+var times = 4;<br>
+<br>
+if (times > 3) {<br>
+    var greeter = "say Hello instead"; <br>
+}<br>
+<br>
+console.log(greeter);<br><br>
+`);
+document.writeln("This code outputs:<br>")
+
+var greeter = "hey hi";
+    var times = 4;
+
+    if (times > 3) {
+        var greeter = "say Hello instead"; 
+    }
+    
+    document.writeln(greeter);
